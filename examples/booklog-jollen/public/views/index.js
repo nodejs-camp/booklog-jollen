5dcb55ce99eda163ac18eb8539ce10b905624f21
Jollen Chen <jollen@moko365.com>
2014-09-28 10:49:20 +0800

    add new backbone view, #16

--- examples/booklog-jollen/public/views/index.js
+++ examples/booklog-jollen/public/views/index.js
@@ -6,6 +6,23 @@
 /**
  * MODELS
  **/
+app.Search = Backbone.Model.extend({  
+  url: function() {
+    return 'http://localhost:3000/1/post/tag/' + this.attributes.tag
+  },
+  tag: '',
+  defaults: {
+    success: false,
+    errors: [],
+    errfor: {},
+    
+    posts: [{
+           "_id": '',
+           "subject": ''
+       }]
+  }
+});
+
 app.Post = Backbone.Model.extend({  
   url: 'http://localhost:3000/1/post',
   defaults: {
@@ -13,17 +30,43 @@ app.Post = Backbone.Model.extend({
     errors: [],
     errfor: {},
     
-	posts: [{
-	       "content": "hello",
-	       "_id": "5402de2f559097cdf139fff9",
-	       "subject": "abc123"
-	   }]
+  	posts: [{
+  	       "content": "hello",
+  	       "_id": "5402de2f559097cdf139fff9",
+  	       "subject": "abc123"
+  	   }]
   }
 });
 
 /**
  * VIEWS
  **/
+  app.SearchView = Backbone.View.extend({
+    el: '#search-section',
+    events: {
+      'click .btn-search': 'performSearch'
+    },
+    initialize: function() {
+        this.model = new app.Search();
+        this.template = _.template($('#tmpl-results').html());
+
+        this.model.bind('change', this.render, this);        
+    },
+    render: function() {
+        var data = this.template(this.model.attributes);
+
+        this.$el.html(data);
+
+        return this;
+    },
+    performSearch: function() {
+      var tag = this.$el.find('#search-tag').val();
+
+      this.model.set('tag', tag);
+      this.model.fetch();
+    }
+  });
+
   app.PostView = Backbone.View.extend({
   	el: '#blog-post',
     events: {
@@ -49,4 +92,5 @@ app.Post = Backbone.Model.extend({
  **/
   $(document).ready(function() {
     app.postView = new app.PostView();
+    app.searchView = new app.SearchView();
   });
