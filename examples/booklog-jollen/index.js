2aaaabc2ac2972356220d31c14de3408417a0b9e
Jollen Chen <jollen@moko365.com>
2014-10-04 13:32:44 +0800

    payment API, #23

--- examples/booklog-jollen/index.js
+++ examples/booklog-jollen/index.js
@@ -30,6 +30,8 @@ var postSchema = new mongoose.Schema({
 
     timeCreated: { type: Date, default: Date.now },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+
+    orders: []
 });
 
 postSchema.index({ content: 'text' });
@@ -104,6 +106,16 @@ passport.use(new FacebookStrategy({
 	   });
   }
 ));
+
+// Paypal
+var paypal_api = require('paypal-rest-sdk');
+
+var config_opts = {
+    'host': 'api.sandbox.paypal.com',
+    'port': '',
+    'client_id': 'AeQjSxAqfJJ_Km70P1rNuO_Tp6C9FWHdDWPsKfEpu8tDmEJpveUheghDcMto',
+    'client_secret': 'ECSbXhDMCHRGU3VYH9-ByQzvhwX7y9ncvNeles7XU9PL2E6d4doHGfvp1BkA'
+};
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
@@ -305,6 +317,68 @@ app.put('/1/post/:postId', function(req, res) {
 
 	res.send("Update a post: " + id);
 });
+
+/**
+ * POST /1/post/:postId/pay
+ */
+app.POST('/1/post/:postId/pay', function(req, res, next) {
+    var workflow = new events.EventEmitter();
+    var postId = req.params.postId;
+    var posts = req.app.db.posts;
+    
+    workflow.outcome = {};
+
+    workflow.on('validate', function() {
+        workflow.emit('createPayment');
+    });
+
+    workflow.on('createPayment', function() {
+		paypal_api.configure(config_opts);
+
+		var create_payment_json = {
+		            intent: 'sale',
+		            payer: {
+		                payment_method: 'paypal'
+		            },
+		            redirect_urls: {
+
+		                // http://localhost:3000/1/post/539eb886e8dbde4b39000007/paid?token=EC-4T17102178173001V&PayerID=QPPLBGBK5ZTVS
+		                return_url: 'https://localhost:3000/1/post/' + 'aaa' + '/paid',
+		                cancel_url: 'https://localhost:3000/1/post/' + 'aaa' + '/cancel'
+		            },
+		            transactions: [{
+		                amount: {
+		                    currency: 'TWD',
+		                    total: 99
+		                },
+		                description: '購買教學文章'
+		            }]
+		};
+
+		paypal_api.payment.create(create_payment_json, function (err, res) {
+		    if (err) {
+		        console.log(err);
+		    }
+
+		    if (res) {
+		        console.log("Create Payment Response");
+		        console.log(res);
+		    }
+
+		    var order = {
+		    	userId: req.user._id,
+		    	paypal: res
+		    };
+
+			posts
+			.update(postId, { $addToSet: { orders: order } }, function(err, post) {
+
+			});
+		});
+    });
+
+    return workflow.emit('validate');
+};
 
 // change this to a better error handler in your code
 // sending stacktrace to users in production is not good
